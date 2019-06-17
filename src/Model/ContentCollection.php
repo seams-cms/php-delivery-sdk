96bedd9a84b632698f6e8f4dd31825ea3cc125ee
Arjan Kleene <arjankleene@gmail.com>
2019-06-17 11:10:49 +0200

    Implement ContentCollection

--- src/Model/ContentCollection.php
+++ src/Model/ContentCollection.php
@@ -6,4 +6,21 @@ namespace SeamsCMS\Delivery\Model;
 
 class ContentCollection extends Collection
 {
+    /** @return Content[] */
+    public function getEntries(): array
+    {
+        return parent::getEntries();
+    }
+
+    public static function fromArray(array $data)
+    {
+        $data['entries'] = array_map(
+            function ($item) {
+                return Content::fromArray($item);
+            },
+            $data['entries']
+        );
+
+        return parent::fromArray($data);
+    }
 }
